10b33583f6fe702549a9bb3707cdd5882ff7d4cf
Hamed <hamedblg29@gmail.com>
2025-11-01 00:04:53 +0100

    Initial commit: reinitialized repository

---
+++ src/components/footer/config.ts
@@ -0,0 +1,12 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Blog",
+    href: "https://hamed.hashnode.dev/",
+  },
+  {
+    title: "Newsletter",
+    href: "https://hamed.hashnode.dev/",
+  },
+];
+
+export { footer };
---
+++ src/data/config.ts
@@ -0,0 +1,42 @@
+const config = {
+  title: "Hamed Bouallagui | Full Stack Developer",
+  description: {
+    long: "Explore the portfolio of Hamed Bouallagui, a passionate Web and Mobile Developer from Sidi Bouzid, Tunisia.Currently pursuing a Bachelor’s degree in Web Development at ISET Sidi Bouzid, I enjoy building modern, responsive, and user-friendly applications that blend functionality with clean design.",
+    short:
+      "Discover the portfolio of Hamed Bouallagui, a AI enthusiast solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "Hamed Bouallagui",
+    "hamedbouallagui",
+    "hamedbouallagui.com",
+    "Hamed Bouallagui portfolio",
+    "portfolio",
+    "Full Stack Developer",
+    "Web Development",
+    "Interactive Websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Hamed Bouallagui",
+  email: "hamedblg29@gmail.com",
+  site: "https://hamedbouallagui.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/Screenshot 2025-10-31 161026.png";
+  },
+  social: {
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/hamed-bouallegui-0320b0288/",
+    instagram: "https://www.instagram.com/hamed_bouallegui/",
+    facebook: "https://www.facebook.com/hame.b.915615",
+    github: "https://github.com/HamedBouallegui",
+  },
+};
+export { config };
